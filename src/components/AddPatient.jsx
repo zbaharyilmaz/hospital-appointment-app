8a4b363e58f82b6067edfbcbe5af3158237d2e70
zeynepyilmaz <ylmzbhr1@gmail.com>
2025-01-09 15:40:27 +0300

    dependencies added & components created & data.js added.

---
+++ src/components/AddPatient.jsx
@@ -0,0 +1,35 @@
+const AddPatient = () => {
+
+   
+  return (
+    <div>
+      <form >
+        <div className="form-control">
+          <label htmlFor="name"> Patient Information</label>
+          <input
+            id="name"
+            type="text"
+
+           
+          />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor=""> Day & Time</label>
+          <input
+            type="datetime-local"
+          
+          />
+        </div>
+
+        <div>
+          <button className="kayitBtn" type="submit">
+            <span>{ } </span> Schedule the Appointment
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default AddPatient;
